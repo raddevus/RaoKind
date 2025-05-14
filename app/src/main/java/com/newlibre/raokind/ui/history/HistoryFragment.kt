972026f520f08270fb4685433759bf7ccd7fc03e
raddevus <1radeutsch1@gmail.com>
2025-05-14 14:27:57 -0400

    Fixed file name

--- app/src/main/java/com/newlibre/raokind/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/newlibre/raokind/ui/history/HistoryFragment.kt
@@ -23,7 +23,7 @@ class HistoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(HistoryViewModel::class.java)
 
         _binding = FragmentHistoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
